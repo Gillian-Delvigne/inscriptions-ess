87a532971a804b7e5fac28eebcb395e6bcf76647
Gillian <gillian.delvigne@gmail.com>
2019-12-18 20:31:58 +0100

    continue form

--- src/app/front-end/body/catalogue/catalogue.component.ts
+++ src/app/front-end/body/catalogue/catalogue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @Component({
   selector: 'app-catalogue',
@@ -16,12 +17,17 @@ export class CatalogueComponent implements OnInit {
   
 }
 
-const ada = {
-  id : 1, 
-  title : "Formation d’animateur à l’atelier citoyenneté",
-  description : "Jour 1 : méthodologie de base d’animation de groupe",
-  durée : "3h30",
-  accessibilité : "Salarié/volontaire qui donne les ateliers citoyenneté dans les centres d’accueil des DPI",
-  Prix : "Gratuit",
-  Contact : "Suley Velaj – suley.velaj@croix-rouge.be"
-};
+
+const formations = [ 
+  {
+    id : 1,
+    type : "ada",
+    title : "Formation d’animateur à l’atelier citoyenneté",
+    description : "Jour 1 : méthodologie de base d’animation de groupe",
+    durée : "3h30",
+    accessibilité : "Salarié/volontaire qui donne les ateliers citoyenneté dans les centres d’accueil des DPI",
+    Prix : "Gratuit",
+    contact : "Suley Velaj – suley.velaj@croix-rouge.be"
+},
+]
+
