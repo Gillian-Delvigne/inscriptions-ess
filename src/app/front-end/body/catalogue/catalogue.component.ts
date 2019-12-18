86504683ec3230f773fa26572e86064ad0bb147f
Gillian <gillian.delvigne@gmail.com>
2019-12-18 18:45:16 +0100

    object-test

--- src/app/front-end/body/catalogue/catalogue.component.ts
+++ src/app/front-end/body/catalogue/catalogue.component.ts
@@ -12,4 +12,16 @@ export class CatalogueComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  
 }
+
+const ada = {
+  id : 1, 
+  title : "Formation d’animateur à l’atelier citoyenneté",
+  description : "Jour 1 : méthodologie de base d’animation de groupe",
+  durée : "3h30",
+  accessibilité : "Salarié/volontaire qui donne les ateliers citoyenneté dans les centres d’accueil des DPI",
+  Prix : "Gratuit",
+  Contact : "Suley Velaj – suley.velaj@croix-rouge.be"
+};
