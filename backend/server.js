642b5eedd5e11611226dae46ff6f4b8022b73e86
Gillian <gillian.delvigne@gmail.com>
2019-12-18 15:20:09 +0100

    backend mysql config test1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const events = require('./events');
 
 const connection = mysql.createConnection({
     host: 'localhost:3306',
@@ -9,12 +12,19 @@ const connection = mysql.createConnection({
     database: 'inscriptions_ess'
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+    .use(cors())
+    .use(bodyParser.json())
+    .use(events(connection));
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
 });
 
-
-app.listen(8000, () => {
+app.listen(8080, () => {
     console.log('Server started!')
 })
