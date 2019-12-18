6e2dc8f6d0decf6f6d3e29c2a1d53a42f853d635
Gillian <gillian.delvigne@gmail.com>
2019-12-18 15:11:36 +0100

    backend mysql config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost:3306',
+    user: 'dg420313',
+    password: '%Iamthewalrus151017-',
+    database: 'inscriptions_ess'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
 
 app.listen(8000, () => {
     console.log('Server started!')
