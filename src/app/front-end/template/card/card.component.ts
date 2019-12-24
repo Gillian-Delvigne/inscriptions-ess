f97c3d91befdd8ecff4fe3627e81e670965bbc02
Gillian <gillian.delvigne@gmail.com>
2019-12-24 13:48:51 +0100

    adding illustrations + ngFor Card

--- src/app/front-end/template/card/card.component.ts
+++ src/app/front-end/template/card/card.component.ts
@@ -12,4 +12,20 @@ export class CardComponent implements OnInit {
   ngOnInit() {
   }
 
+  trainings = [
+    {
+        name: "Au coeur de la Croix-Rouge",
+        description: "Ce module participatif et convivial te permet de situer ton action au sein du Mouvement international de la Croix-Rouge et du Croissant-Rouge, et à travers des cas pratiques, de mesurer la portée concrète des Principes fondamentaux et des valeurs humanitaires dans ton engagement volontaire."
+        + "\n" + "Le module vise également à mieux comprendre l’organisation et le fonctionnement de la Croix-Rouge de Belgique, ses activités, l’organisation du volontariat, etc. notamment à travers les échanges avec d’autres volontaires issu·es d’activités et d’entités variées.",
+        picto_url:"././././assets/img/pictos/pour-tous-3.png",
+        image_url:"././././assets/img/illustrations/formation-au-coeur-crb.png",
+      },
+
+    {
+        name: 'Clean Code'
+    }
+];
+
+
+
 }
